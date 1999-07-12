02449e63cf99561e12b04cdfb52d0c80cc331e89
Caolán McNamara <caolan@skynet.ie>
1999-07-12 10:56:41 +0000

    added strcasecmp.c
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/wv/trunk@20255 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ strcasecmp.c
@@ -0,0 +1,13 @@
+#include <ctype.h>
+#include <string.h>
+
+int strcasecmp(const char *s1, const char *s2)
+	{
+	int i;
+	if (strlen(s1) != strlen(s1))
+		return(1);
+	for (i=0;i<strlen(s1);i++)
+		if ( toupper(s1[i]) != toupper(s2[i]) )
+			return(1);
+	return(0);		
+	}
