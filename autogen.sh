5379f9f6209034afc8f4529f7c497efeebc338d7
Francis J Franklin <fjf@svn.abisource.com>
2002-06-11 15:58:32 +0000

    o simplify autogen.sh & update configure.ac to support glib2
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/wv/trunk@20618 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- autogen.sh
+++ autogen.sh
@@ -1,53 +1,6 @@
 #!/bin/sh
 
-if test "x$SHELL" = "x"; then
-  SHELL=/bin/sh
-  export SHELL
-fi
-
-current_path=`echo $PATH | sed 's/:/ /g'`
-for i in $current_path; do
-  if test -d $i; then
-    if test "$LIBTOOLIZE" = ""; then
-      if test -x $i/libtoolize; then
-        LIBTOOLIZE=$i/libtoolize
-        export LIBTOOLIZE
-        echo "Found: $LIBTOOLIZE"
-      fi
-    fi
-    if test "$AUTOCONF" = ""; then
-      if test -x $i/autoconf; then
-        AUTOCONF=$i/autoconf
-        export AUTOCONF
-        echo "Found: $AUTOCONF"
-      fi
-    fi
-    if test "$AUTOHEADER" = ""; then
-      if test -x $i/autoheader; then
-        AUTOHEADER=$i/autoheader
-        export AUTOHEADER
-        echo "Found: $AUTOHEADER"
-      fi
-    fi
-  else
-    echo "warning: non-directory or unexpected characters in PATH"
-    echo "- found: '$i'"
-  fi
-done
-if test "$LIBTOOLIZE" = ""; then
-  echo "Can't find libtoolize"
-  exit 1
-fi
-if test "$AUTOCONF" = ""; then
-  echo "Can't find autoconf"
-  exit 1
-fi
-if test "$AUTOHEADER" = ""; then
-  echo "Can't find autoheader"
-  exit 1
-fi
-
-accheck=`$SHELL $AUTOCONF --version | grep 2.13`
+accheck=`autoconf --version | grep 2.13`
 if test "x$accheck" != "x"; then
     echo "error: you appear to be using autoconf 2.13"
     echo "       the automake build system requires autoconf >= 2.50"
@@ -69,22 +22,22 @@ if test "x$amcheck" = "xautomake (GNU automake) 1.5"; then
     echo "         this version has a bug - GNUmakefile.am dependencies are not generated"
 fi
 
-$SHELL $LIBTOOLIZE --force --copy || {
+libtoolize --force --copy || {
     echo "error: libtoolize failed"
     exit 1
 }
-aclocal || {
-    echo "error: aclocal failed"
+aclocal $ACLOCAL_FLAGS || {
+    echo "error: aclocal $ACLOCAL_FLAGS failed"
     exit 1
 }
-$SHELL $AUTOHEADER || {
+autoheader || {
     echo "error: autoheader failed"
     exit 1
 }
-automake -a --foreign || {
+automake -a -c --foreign || {
     echo "warning: automake failed"
 }
-$SHELL $AUTOCONF || {
+autoconf || {
     echo "error: autoconf failed"
     exit 1
 }
