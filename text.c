6139a675549ba1104108bfb4dc7e6477e5c55993
Dom Lachowicz <domlachowicz@gmail.com>
2007-05-12 01:41:34 +0000

    bug 10961 - characters in word files show as '?'s. this is because various iconv deployments (especially on embedded linux platforms) don't include support for cp1252, which the majority of western european word documents use. work around this by special-casing cp1252 and including our own cp1252 to ucs-2 table. this should also be faster than using iconv to convert 1 character at a time.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/wv/trunk@21291 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- text.c
+++ text.c
@@ -30,6 +30,44 @@
 
 int (*wvConvertUnicodeToEntity) (U16 char16) = NULL;
 
+/* enough word docs use the cp1252 encoding and enough iconv
+ * implementations don't include it that it's worth special-
+ * casing */
+static const U16 cp1252_to_ucs2_table[] = {
+  0x0000, 0x0001, 0x0002, 0x0003, 0x0004, 0x0005, 0x0006, 0x0007
+, 0x0008, 0x0009, 0x000A, 0x000B, 0x000C, 0x000D, 0x000E, 0x000F
+, 0x0010, 0x0011, 0x0012, 0x0013, 0x0014, 0x0015, 0x0016, 0x0017
+, 0x0018, 0x0019, 0x001A, 0x001B, 0x001C, 0x001D, 0x001E, 0x001F
+, 0x0020, 0x0021, 0x0022, 0x0023, 0x0024, 0x0025, 0x0026, 0x0027
+, 0x0028, 0x0029, 0x002A, 0x002B, 0x002C, 0x002D, 0x002E, 0x002F
+, 0x0030, 0x0031, 0x0032, 0x0033, 0x0034, 0x0035, 0x0036, 0x0037
+, 0x0038, 0x0039, 0x003A, 0x003B, 0x003C, 0x003D, 0x003E, 0x003F
+, 0x0040, 0x0041, 0x0042, 0x0043, 0x0044, 0x0045, 0x0046, 0x0047
+, 0x0048, 0x0049, 0x004A, 0x004B, 0x004C, 0x004D, 0x004E, 0x004F
+, 0x0050, 0x0051, 0x0052, 0x0053, 0x0054, 0x0055, 0x0056, 0x0057
+, 0x0058, 0x0059, 0x005A, 0x005B, 0x005C, 0x005D, 0x005E, 0x005F
+, 0x0060, 0x0061, 0x0062, 0x0063, 0x0064, 0x0065, 0x0066, 0x0067
+, 0x0068, 0x0069, 0x006A, 0x006B, 0x006C, 0x006D, 0x006E, 0x006F
+, 0x0070, 0x0071, 0x0072, 0x0073, 0x0074, 0x0075, 0x0076, 0x0077
+, 0x0078, 0x0079, 0x007A, 0x007B, 0x007C, 0x007D, 0x007E, 0x007F
+, 0x20AC, 0x0000, 0x201A, 0x0192, 0x201E, 0x2026, 0x2020, 0x2021
+, 0x02C6, 0x2030, 0x0160, 0x2039, 0x0152, 0x0000, 0x017D, 0x0000
+, 0x0000, 0x2018, 0x2019, 0x201C, 0x201D, 0x2022, 0x2013, 0x2014
+, 0x02DC, 0x2122, 0x0161, 0x203A, 0x0153, 0x0000, 0x017E, 0x0178
+, 0x00A0, 0x00A1, 0x00A2, 0x00A3, 0x00A4, 0x00A5, 0x00A6, 0x00A7
+, 0x00A8, 0x00A9, 0x00AA, 0x00AB, 0x00AC, 0x00AD, 0x00AE, 0x00AF
+, 0x00B0, 0x00B1, 0x00B2, 0x00B3, 0x00B4, 0x00B5, 0x00B6, 0x00B7
+, 0x00B8, 0x00B9, 0x00BA, 0x00BB, 0x00BC, 0x00BD, 0x00BE, 0x00BF
+, 0x00C0, 0x00C1, 0x00C2, 0x00C3, 0x00C4, 0x00C5, 0x00C6, 0x00C7
+, 0x00C8, 0x00C9, 0x00CA, 0x00CB, 0x00CC, 0x00CD, 0x00CE, 0x00CF
+, 0x00D0, 0x00D1, 0x00D2, 0x00D3, 0x00D4, 0x00D5, 0x00D6, 0x00D7
+, 0x00D8, 0x00D9, 0x00DA, 0x00DB, 0x00DC, 0x00DD, 0x00DE, 0x00DF
+, 0x00E0, 0x00E1, 0x00E2, 0x00E3, 0x00E4, 0x00E5, 0x00E6, 0x00E7
+, 0x00E8, 0x00E9, 0x00EA, 0x00EB, 0x00EC, 0x00ED, 0x00EE, 0x00EF
+, 0x00F0, 0x00F1, 0x00F2, 0x00F3, 0x00F4, 0x00F5, 0x00F6, 0x00F7
+, 0x00F8, 0x00F9, 0x00FA, 0x00FB, 0x00FC, 0x00FD, 0x00FE, 0x00FF
+};
+
 U16
 wvnLocaleToLIDConverter (U8 nLocale)
 {
@@ -393,11 +431,55 @@ wvLIDToLangConverter (U16 lid)
 	return "-none-"; /* default */
 }
 
+static int
+wvIsCP1252 (U16 lid)
+{
+  switch (lid & 0xff)
+    {
+      case 0x03:		/*Catalan */
+      case 0x06:		/*Danish */
+      case 0x07:		/*German */
+      case 0x09:		/*English */
+      case 0x0a:		/*Spanish */
+      case 0x0b:		/*Finnish */
+      case 0x0c:		/*French */
+      case 0x0f:		/*Icelandic */
+      case 0x10:		/*Italian */
+      case 0x13:		/*Dutch */
+      case 0x14:		/*Norwegian */
+      case 0x16:		/*Portuguese */
+      case 0x17:		/*Rhaeto-Romanic */
+      case 0x1d:		/*Swedish */
+      case 0x21:		/*Bahasa Indonesian */
+      case 0x2d:		/*Basque */
+      case 0x36:		/*Afrikaans */
+      case 0x38:		/*Faeroese */
+      case 0x3E:		/*Malaysian / Malay */
+      case 0x41:		/*Swahili */
+	return 1;
+
+      case 0x1a:		/*Serbian, Croatian, (Bosnian?) */
+	switch (lid)
+	  {
+	  case 0x041a:		/*Croatian */
+	  case 0x081a:		/*Serbian (Latin) */
+	    return 1;
+	  }
+
+    default:
+      return 0;
+    }
+
+  return 0;
+}
+
 const char *
 wvLIDToCodePageConverter (U16 lid)
 {
     if (lid == 0x0FFF)	/*Macintosh Hack */
 	  return ("MACINTOSH");
+    else if (wvIsCP1252 (lid))
+      return ("CP1252");
 
     switch (lid & 0xff)
 	  {
@@ -405,8 +487,6 @@ wvLIDToCodePageConverter (U16 lid)
 	  return ("CP1256");
       case 0x02:		/*Bulgarian */
 	  return ("CP1251");
-      case 0x03:		/*Catalan */
-	  return ("CP1252");
       case 0x04:		/*Chinese */
       switch (lid)
 		{
@@ -425,28 +505,12 @@ wvLIDToCodePageConverter (U16 lid)
 		}
       case 0x05:		/*Czech */
 	  return ("CP1250");
-      case 0x06:		/*Danish */
-	  return ("CP1252");
-      case 0x07:		/*German */
-	  return ("CP1252");
       case 0x08:		/*Greek */
 	  return ("CP1253");
-      case 0x09:		/*English */
-	  return ("CP1252");
-      case 0x0a:		/*Spanish */
-	  return ("CP1252");
-      case 0x0b:		/*Finnish */
-	  return ("CP1252");
-      case 0x0c:		/*French */
-	  return ("CP1252");
       case 0x0d:		/*Hebrew */
 	  return ("CP1255");
       case 0x0e:		/*Hungarian */
 	  return ("CP1250");
-      case 0x0f:		/*Icelandic */
-	  return ("CP1252");
-      case 0x10:		/*Italian */
-	  return ("CP1252");
       case 0x11:		/*Japanese */
 	  return ("CP932");
       case 0x12:		/*Korean */
@@ -457,16 +521,8 @@ wvLIDToCodePageConverter (U16 lid)
       	case 0x0412:		/*Korean */
 	  	return ("CP949");
 		}
-      case 0x13:		/*Dutch */
-	  return ("CP1252");
-      case 0x14:		/*Norwegian */
-	  return ("CP1252");
       case 0x15:		/*Polish */
 	  return ("CP1250");
-      case 0x16:		/*Portuguese */
-	  return ("CP1252");
-      case 0x17:		/*Rhaeto-Romanic */
-	  return ("CP1252");
       case 0x18:		/*Romanian */
 	  return ("CP1250");
       case 0x19:		/*Russian */
@@ -474,27 +530,19 @@ wvLIDToCodePageConverter (U16 lid)
       case 0x1a:		/*Serbian, Croatian, (Bosnian?) */
       switch (lid)
 		{
-      	case 0x041a:		/*Croatian */
-	  	return ("CP1252");
       	case 0x0c1a:		/*Serbian (Cyrillic) */
 	  	return ("CP1251");
-      	case 0x081a:		/*Serbian (Latin) */
-	  	return ("CP1252");
 		}
       case 0x1b:		/*Slovak */
 	  return ("CP1250");
       case 0x1c:		/*Albanian */
 	  return ("CP1251");
-      case 0x1d:		/*Swedish */
-	  return ("CP1252");
       case 0x1e:		/*Thai */
 	  return ("CP874");
       case 0x1f:		/*Turkish */
 	  return ("CP1254");
       case 0x20:		/*Urdu. This is Unicode only. */
-	  return ("0");
-      case 0x21:		/*Bahasa Indonesian */
-	  return ("CP1252");
+	  return ("CP0");
       case 0x22:		/*Ukrainian */
 	  return ("CP1251");
       case 0x23:		/*Byelorussian / Belarusian */
@@ -508,7 +556,7 @@ wvLIDToCodePageConverter (U16 lid)
       case 0x27:		/*Lithuanian */
 	  return ("CP1257");
       case 0x29:		/*Farsi / Persian. This is Unicode only. */
-	  return ("0");
+	  return ("CP0");
       case 0x2a:		/*Vietnamese */
 	  return ("CP1258");
       case 0x2b:		/*Windows 2000: Armenian. This is Unicode only. */
@@ -522,28 +570,18 @@ wvLIDToCodePageConverter (U16 lid)
       	case 0x042c:		/*Azeri (Latin) */
 #endif
 		}
-      case 0x2d:		/*Basque */
-	  return ("CP1252");
       case 0x2f:		/*Macedonian */
 	  return ("CP1251");
 #if 0
       case 0x30:		/*Sutu */
 #endif
-      case 0x36:		/*Afrikaans */
-	  return ("CP1252");
       case 0x37:		/*Windows 2000: Georgian. This is Unicode only. */
 	  return ("CP0");
-      case 0x38:		/*Faeroese */
-	  return ("CP1252");
       case 0x39:		/*Windows 2000: Hindi. This is Unicode only. */
 	  return ("CP0");
-      case 0x3E:		/*Malaysian / Malay */
-	  return ("CP1252");
 #if 0
       case 0x3f:		/*Kazakh */
 #endif
-      case 0x41:		/*Swahili */
-	  return ("CP1252");
       case 0x43:		/*Uzbek */
       switch (lid)
 		{
@@ -588,10 +626,10 @@ wvLIDToCodePageConverter (U16 lid)
 static U32
 swap_iconv (U16 lid)
 {
-    GIConv handle = NULL;
+    GIConv handle;
     char f_code[33];		/* From CCSID                           */
     char t_code[33];		/* To CCSID                             */
-    const char *codepage = NULL;
+    const char *codepage;
     size_t ibuflen, obuflen;
 
     U8 buffer[2];
@@ -654,6 +692,11 @@ wvHandleCodePage (U16 eachchar, U16 lid)
     U8 buffer2[2];
 
     U16 rtn;
+
+    if (wvIsCP1252 (lid) && eachchar <= 0xFF)
+      {
+	return cp1252_to_ucs2_table[eachchar];
+      }
 
     if (eachchar > 0xff)
       {
