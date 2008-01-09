33cb642dcc51f1343f195c4ebcbe1e5a3fc5eb5b
Rob Staudinger <robert.staudinger@gmail.com>
2008-01-09 15:34:13 +0000

    Win32 crossbuild: link dll
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/wv/trunk@22626 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- autogen.sh
+++ autogen.sh
@@ -23,18 +23,19 @@ automake --version > /dev/null 2> /dev/null || {
     exit 1
 }
 
-automake --version | perl -ne 'if (/\(GNU automake\) ([0-9].[0-9])/) {print;  if ($1 < 1.6) {exit 1;}}'
+automake --version | perl -ne 'if (/\(GNU automake\) (([0-9]+).([0-9]+))/) {print; if ($2 < 1 || ($2 == 1 && $3 < 4)) {exit 1;}}'
 if [ $? -ne 0 ]; then
     echo "warning: you appear to be using automake <= 1.5"
     echo "         these versions have bugs - GNUmakefile.am dependencies are not generated"
 fi
 
-libtoolize --force --copy || {
-    echo "error: libtoolize failed"
-    exit 1
-}
 aclocal $ACLOCAL_FLAGS || {
     echo "error: aclocal $ACLOCAL_FLAGS failed"
+    exit 1
+}
+
+libtoolize --force --copy || {
+    echo "error: libtoolize failed"
     exit 1
 }
 
